01fed695d59ffe2f3269fff7d8184c83aaba6704
Daniel <danielngozikac@gmail.com>
2022-04-13 12:52:48 +0100

    Import React to fix React scope error.

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "./style.module.css";
 
 const Countdown = ({ timer }) => {
--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Logo from "../../assests/logo.jpg";
 import styles from "./style.module.css";
 
